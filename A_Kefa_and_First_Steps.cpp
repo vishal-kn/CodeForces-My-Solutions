ecce089a0516dc65a743f926d43657a96c774bbe
vishal-kn <89460309+vishal-kn@users.noreply.github.com>
2021-08-24 19:18:29 +0530

    Update A_Kefa_and_First_Steps.cpp

--- A_Kefa_and_First_Steps.cpp
+++ A_Kefa_and_First_Steps.cpp
@@ -1,3 +1,4 @@
+//##########################   SOLUTION 1   ##############################
 #include <bits/stdc++.h>
 using namespace std;
 int main()
@@ -16,5 +17,34 @@ int main()
             c++;
         ans=max(ans,c);
     }
+    cout<<ans<<endl;
+}
+
+//##########################   SOLUTION 2   ##############################
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n,i,j,maxl,count,diff,ans;
+    cin>>n;
+    int a[n];
+    for(i=0;i<n;i++)
+    cin>>a[i];
+    maxl=count=0;
+    for(i=0,j=0;j<n;)
+    {
+        cin>>a[i];
+        if(a[j+1]<a[j])
+        {
+            j++;
+            i=j;
+            maxl=count;
+            continue;
+        }
+        count++;
+        j++;
+        ans=max(count,maxl);
+    }
+    
     cout<<ans<<endl;
 }
