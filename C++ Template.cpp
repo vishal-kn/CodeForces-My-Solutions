40335e69e7f1a0381e546269a93efd8a117e6223
Vishal Kanoujiya <89460309+vishal-kn@users.noreply.github.com>
2021-08-25 11:52:10 +0530

    Create C++ Template.cpp

---
+++ C++ Template.cpp
@@ -0,0 +1,35 @@
+
+#include <bits/stdc++.h>
+typedef long long ll;
+
+#define for0(i, n) for (int i = 0; i < (int)(n);i+) // 0 based indexing
+#define for1(i, n) for (int i = 1; i <= (int)(n);i++) // 1 based indexing
+#define forc(i, l, r) for (int i = (int)(l); i <= (int)(r); ++i) // closed interver from l to r r inclusive
+#define forr0(i,n) for(int i=(n);i>=0;i--)// reverse 0 based indexing
+#define forr1(i,n) for(int i=(n);i>0;i--)// reverse 1 based indexing
+
+//short hand for usual tokens
+#define pb push_back
+#define fi first
+#define se second
+
+// to be used with algorithms that processes a container Eg: find(all(c),42)
+#define all(x) (x).begin(), (x).end() //Forward traversal
+#define rall(x) (x).rbegin, (x).rend() //reverse traversal
+
+using namespace std;
+
+int main()
+{
+   ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+	  int tc;
+	  cin>>tc;
+    while(tc--)
+    solve();
+    return 0;
+}
+void solve()
+{
+
+}
